e565389775eba2917ba14f507e45c1e731923385
Timothy Charboneau <69163325+charboneaut@users.noreply.github.com>
2021-10-09 21:27:47 -0400

    replaced template w/ text input

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [text, changeText] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div onChange={console.log(text)} className="App">
+      <h1>Input Text to modified</h1>
+      <input value={text} onChange={(e) => changeText(e.target.value)} />
     </div>
   );
 }
